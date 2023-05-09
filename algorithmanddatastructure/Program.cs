3dc51996f1da39ef5e92ec0dd4e5f90b599981c9
Yohannes Tesfaye <joguyjohna@gmail.com>
2023-05-09 23:06:29 +0300

    two sum

--- algorithmanddatastructure/Program.cs
+++ algorithmanddatastructure/Program.cs
@@ -1,29 +1,20 @@
 ﻿
 
-
-
-List<int> candles = new List<int> { 3  , 2, 1, 3 };
-
-int temp;
-for (int i = 0; i < candles.Count - 1; i++)
+int[] nums = new int [] {3,2,3};
+int target = 6;
+int[] result = new int[2];
+List<int> termsList = new List<int>();
+for (int i = 0; i < nums.Count() - 1; i++)
 {
-    for (int j = i + 1; j < candles.Count; j++)
+   for (int j = i+1; j < nums.Count(); j++)
     {
-        if (candles[i] < candles[j])
+        if (nums[i] + nums[j] == target)
         {
-            temp = candles[i];
-            candles[i] = candles[j];
-            candles[j] = temp;
+            termsList.Add(i);
+            termsList.Add(j);
         }
+        result = termsList.ToArray();
     }
 }
-int num = candles.FirstOrDefault();
-int count = candles.Count(x => x == num);
-Console.WriteLine(count);
-
-List<int> list = new List<int> { 5, 7, 3 };
-list.Sort();
-foreach (int x in list)
-{
-    Console.WriteLine(x);
-}
+Console.WriteLine(result[0]);
+Console.WriteLine(result[1]);
